35194de74ae6f31f2e0128717744c5f88b2c9bca
Glynn Bird <glynn.bird@gmail.com>
2023-04-19 11:05:32 +0100

    minify

--- code/build.sh
+++ code/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npx rollup --format=es --file=dist/add_todo.js -- add_todo.js
-npx rollup --format=es --file=dist/delete_todo.js -- delete_todo.js
-npx rollup --format=es --file=dist/get_todo.js -- get_todo.js
-npx rollup --format=es --file=dist/list_todos.js -- list_todos.js
+npx rollup -p @rollup/plugin-terser --format=es --file=dist/add_todo.js -- add_todo.js
+npx rollup -p @rollup/plugin-terser --format=es --file=dist/delete_todo.js -- delete_todo.js
+npx rollup -p @rollup/plugin-terser --format=es --file=dist/get_todo.js -- get_todo.js
+npx rollup -p @rollup/plugin-terser --format=es --file=dist/list_todos.js -- list_todos.js
