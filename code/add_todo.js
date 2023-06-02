c7ac25888761bad0d164b3e039d87f36e7ff0a33
Glynn Bird <glynn.bird@gmail.com>
2023-06-02 14:20:34 +0100

    generated add_todo

--- code/add_todo.js
+++ code/add_todo.js
@@ -1,31 +1,34 @@
 import { okResponse, notOkResponse, notOk } from './lib/constants.js'
+import { handleCORS, mustBePOST, mustBeJSON, apiKey } from './lib/checks.js'
 
-export default {
-  async fetch (request, env, ctx) {
+export async function onRequest(context) {
+  // handle CORS/POST/JSON/apikey chcecks
+  const r = handleCORS(context.request) || apiKey(context.request, context.env) || mustBePOST(context.request) || mustBeJSON(context.request)
+  if (r) return r
 
-    // parse the json
-    const json = await request.json()
+  // parse the json
+  const json = await context.request.json()
 
-    // if there's a title
-    if (json.title) {
-      // create a time-based key
-      const key = (new Date().getTime()).toString()
+  // if there's a title
+  if (json.title) {
+    // create a time-based key
+    const key = (new Date().getTime()).toString()
 
-      // write key/value to the KV store, bound to this worker as TODOLIST
-      const metadata = {
-        time: new Date().toISOString(),
-        title: json.title,
-        description: json.description
-      }
-      // put the data in "metadata" instead of value, so that it comes back
-      // in the .list() request
-      await env.TODOLIST.put(key, null, { metadata })
-
-      // send response
-      return new Response(JSON.stringify({ ok: true, id: key }), okResponse)
+    // write key/value to the KV store, bound to this worker as TODOLIST
+    const metadata = {
+      time: new Date().toISOString(),
+      title: json.title,
+      description: json.description
     }
-    
-    // everyone else gets a 400 response
-    return new Response(notOk, notOkResponse)
+    // put the data in "metadata" instead of value, so that it comes back
+    // in the .list() request
+    await context.env.TODOLIST.put(key, null, { metadata })
+
+    // send response
+    return new Response(JSON.stringify({ ok: true, id: key }), okResponse)
   }
+  
+  // everyone else gets a 400 response
+  return new Response(notOk, notOkResponse)
+
 }
--- code/build.sh
+++ code/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#npx rollup -p @rollup/plugin-terser --format=es --file=../functions/add_todo.js -- add_todo.js
-#npx rollup -p @rollup/plugin-terser --format=es --file=../functions/delete_todo.js -- delete_todo.js
-#npx rollup -p @rollup/plugin-terser --format=es --file=../functions/get_todo.js -- get_todo.js
-#npx rollup -p @rollup/plugin-terser --format=es --file=../functions/list_todos.js -- list_todos.js
+npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/add_todo.js -- add_todo.js
+# npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/delete_todo.js -- delete_todo.js
+# npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/get_todo.js -- get_todo.js
+# npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/list_todos.js -- list_todos.js
 #npx rollup --format=es --file=dist/router.js -- router.js
